3732ce16f93f307e1d305abe2dc2654698a2f4a6
Willian Eduardo <wieduardo13@hotmail.com>
2023-04-10 09:42:10 -0300

    Alteração de foto

--- src/components/Intro/index.jsx
+++ src/components/Intro/index.jsx
@@ -4,10 +4,10 @@ function Intro(){
     return(
         <>
         <main>
-            <img className='bola' src='https://hscontabil.com.br/wp-content/uploads/2019/11/contador.jpg' alt='contador'/>
+            <img className='bola' src='https://pps.whatsapp.net/v/t61.24694-24/286868023_1102867177251460_7501808139272682249_n.jpg?ccb=11-4&oh=01_AdTw33rQlAlbloi0Bp5r5TFp3BOmMA4kUODmsEFp-YXvhQ&oe=644106AA' alt='contador'/>
             <text>
             Contador, Economista. Administrador de Empresas, com 40 anos de experiência.
-            Oferece serviços  a MEIs, Micro Empresas e Empresas de Pequeno Porte tudo via teleprocessamento.
+            Oferece serviços  a MEIs, Micro Empresas e Empresas de Pequeno Porte.
             Sem burocracia, tudo via teleprocessamento:
             Contabilidade, Fiscal, Registro de Funcionários e folha de pagamento.
             </text>
