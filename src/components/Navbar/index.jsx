006ffc08d27199e13baa33dc67998834d2126462
Willian Eduardo <wieduardo13@hotmail.com>
2023-04-10 13:06:28 -0300

    Nome nav alteração

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -9,7 +9,7 @@ function Navbar(){
     return(
         <>
         <nav>
-            <h1>L.C Santana</h1>
+            <h1>LCS Contabilidade</h1>
             <a target="_blank" rel='noreferrer'
                 href={`https://api.whatsapp.com/send?phone=+55${userData.whatsappNumber}&text=Ol%C3%A1%2C%20venho%20por%20meio%20do%20seu%20portf%C3%B3lio%20na%20internet%2C%20gostaria%20de%20conhecer%20melhor%20seus%20servi%C3%A7os`}>Whatsapp</a>
         </nav>
