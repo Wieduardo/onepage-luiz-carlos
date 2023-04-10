d92a312ef323e1180e910e44bea84f22b3448fa1
Willian Eduardo <wieduardo13@hotmail.com>
2023-04-10 13:41:12 -0300

    Alterações de dados

--- src/components/Servicos/index.jsx
+++ src/components/Servicos/index.jsx
@@ -3,20 +3,15 @@ import './style.css';
 function Servicos(){
 
     const servicos=[
-        {name:"Abertura e encerramento de empresa" , descri: "Calculo e gerenciamento de despesas.", img:"https://hscontabil.com.br/wp-content/uploads/2019/11/contador.jpg"},
-        {name:"Impostos" , descri: "Cálculo de imposto de acordo com o perfil econômico.", img:"https://hscontabil.com.br/wp-content/uploads/2019/11/contador.jpg"},
-        {name:"Obrigações fiscais" , descri: "Contabeis e de Recursos Humanos.", img:"https://hscontabil.com.br/wp-content/uploads/2019/11/contador.jpg"},
-        {name:"Terceirização" , descri: "De sua area administrativa.", img:"https://hscontabil.com.br/wp-content/uploads/2019/11/contador.jpg"},
-        {name:"Planejamento Estrategico" , descri: "Montagem e acompanhamento mensal.", img:"https://hscontabil.com.br/wp-content/uploads/2019/11/contador.jpg"},
-        {name:"Planejamento Fiscal" , descri: "Com o melhor enquadramento conforme o porte e  a caracteristica fiscal de cada empresa", img:"https://hscontabil.com.br/wp-content/uploads/2019/11/contador.jpg"},
-        {name:"Planejamento Operacional" , descri: "Com projeção e acompanhamento de aplicabilidade.", img:"https://hscontabil.com.br/wp-content/uploads/2019/11/contador.jpg"},
-        {name:"Revisão de Situação Fiscal" , descri: "Parcelamentos e contestações.", img:"https://hscontabil.com.br/wp-content/uploads/2019/11/contador.jpg"}
-    ]
+        {name:"Assessoria" , descri: "Desde a criação da conta Gov até o acesso ao aplicativo Gov.br.", img:"https://hscontabil.com.br/wp-content/uploads/2019/11/contador.jpg"},
+        {name:"Alteração de CNPJ MEI" , descri: "Para atualização ou acréscimo de atividades.", img:"https://hscontabil.com.br/wp-content/uploads/2019/11/contador.jpg"},
+        {name:"Baixa do CNPJ MEI" , descri: "Não receba cobranças desnecessárias, cancele o CNPJ MEI, ainda que tenha débitos.", img:"https://hscontabil.com.br/wp-content/uploads/2019/11/contador.jpg"},
+        ]
 
     return(
         <>
         <div className='container'>
-            <h2>Meus Serviços</h2>
+            <h2>Serviços</h2>
             <div className='servicos_container'>
             {servicos.map((servico, index) => (
                 <div key={index} className="servico_card">
