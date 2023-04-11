7d06d1a02f53666e80b6e2b2230cb0496f22aecd
Willian Eduardo <wieduardo13@hotmail.com>
2023-04-11 16:09:28 -0300

    Ajustes css

--- src/components/Servicos/index.jsx
+++ src/components/Servicos/index.jsx
@@ -3,14 +3,22 @@ import './style.css';
 function Servicos(){
 
     const servicos=[
-        {   name:"Assessoria MEIs" ,
-            name2:"Alteração de CNPJ MEI",
-            name3:"Baixa do CNPJ MEI",
-            descri: "Desde a criação da conta Gov até o acesso ao aplicativo Gov.br.",
-            descri2:"Contabilidade, Fiscal, Registro de Funcionários, Folha de Pagamento e Encerramento de Empresa.",
-            descri3: "Terceirização da área administrativa - Planejamento Estratégico, montagem e acompanhamento mensal.",
+        {
+            name:"Assessoria MEIs" ,
+            descri:"Criação da conta Gov até o acesso ao aplicativo gov.br; Alteração  e/ou Baixa de CNPJ MEI, cancele seu CNPJ MEI para evitar cobranças desnecessárias ainda que tenha dívidas.",
             img:"https://hscontabil.com.br/wp-content/uploads/2019/11/contador.jpg"
-        }]
+        },
+        {
+            name:"Abertura de Empresa" ,
+            descri:"Contabilidade Fiscal, Registro de Funcionários; Folha de Pagamento e Encerramento de Empresa.",
+            img:"https://hscontabil.com.br/wp-content/uploads/2019/11/contador.jpg"
+        },
+        {
+            name:"Gestão Administrativa" ,
+            descri:"Terceirização dea área administrativa; Planejamento estratégico, montagem e acompanhamento mensal.",
+            img:"https://hscontabil.com.br/wp-content/uploads/2019/11/contador.jpg"
+        }
+    ]
 
     return(
         <>
@@ -23,10 +31,6 @@ function Servicos(){
                     <div className="servico_content">
                         <h3>{servico.name}</h3>
                         <span>{servico.descri}</span>
-                        <h4>{servico.name2}</h4>
-                        <span>{servico.descri2}</span>
-                        <h4>{servico.name3}</h4>
-                        <span>{servico.descri3}</span>
                     </div>
                 </div>))
             }
